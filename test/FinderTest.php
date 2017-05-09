1056e560eb2faed0dae5d4f769d58047a3324768
francoisDOTfr <f@francois.fr>
2017-05-09 16:39:20 +0200

    hop

--- test/FinderTest.php
+++ test/FinderTest.php
@@ -9,6 +9,31 @@ use \SimilarText\Finder;
 class FinderTest extends \PHPUnit_Framework_TestCase
 {
     public function testOk() {
-        $this->assertTrue(true);
+        //$this->assertTrue(true);
+
+        $a = new Finder("bananne",["kiwi","banane"]);
+        //echo $a->first();
+
+        $this->assertEquals(
+            'banane',
+            $a->first()
+        );
+
+       	$this->assertEquals(
+            'banane',
+            $a->first()
+        );
+    }
+
+    public function testOk2() {
+        //$this->assertTrue(true);
+
+        $a = new Finder("bananne",["kiwi","banane"]);
+        //echo $a->first();
+
+        $this->assertEquals(
+            'banane',
+            $a->first()
+        );
     }
 }
